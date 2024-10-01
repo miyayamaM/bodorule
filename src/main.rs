dc0e4f7418e365f1a24b6187a4a86931b18e8b54
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-10-01 20:49:27 +0900

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-fn main() {
-    println!("g: {}", std::env::var("GLOBAL").unwrap());
-    println!("l: {}", std::env::var("LOCAL").unwrap());
+use std::net::{Ipv4Addr, SocketAddr};
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello", get(hello_world));
+
+    let address = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+
+    let listener = TcpListener::bind(address).await.unwrap();
+    println!("listening on {}", address);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
 }
