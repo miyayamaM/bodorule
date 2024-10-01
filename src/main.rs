b1eddaa3c978739d5f4507e6cdcf111e3c736502
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-10-01 21:01:02 +0900

    feat: healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/hello", get(hello_world));
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health", get(health_check));
 
     let address = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
 
-    let listener = TcpListener::bind(address).await.unwrap();
+    let listener = TcpListener::bind(address).await?;
     println!("listening on {}", address);
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
 
-async fn hello_world() -> &'static str {
-    "Hello, World!"
+async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
