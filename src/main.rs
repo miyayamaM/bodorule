34869dc65a950754c7ee3bc04c1efa10381cc3de
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-10-02 20:01:52 +0900

    Databaseへ接続

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use anyhow::Result;
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{extract::State, http::StatusCode, routing::get, Router};
+use sqlx::{postgres::PgConnectOptions, PgPool};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().route("/health", get(health_check));
+    let cfg = DatabaseConfig {
+        host: "localhost".to_string(),
+        port: 5432,
+        username: "app".to_string(),
+        password: "passwd".to_string(),
+        database: "app".to_string(),
+    };
+    let pool = connect_database_with(cfg);
+
+    let app = Router::new()
+        .route("/health", get(health_check))
+        .with_state(pool);
 
     let address = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
 
@@ -15,6 +27,38 @@ async fn main() -> Result<()> {
     Ok(axum::serve(listener, app).await?)
 }
 
-async fn health_check() -> StatusCode {
-    StatusCode::OK
+struct DatabaseConfig {
+    host: String,
+    port: u16,
+    username: String,
+    password: String,
+    database: String,
 }
+
+impl From<DatabaseConfig> for PgConnectOptions {
+    fn from(cfg: DatabaseConfig) -> Self {
+        PgConnectOptions::new()
+            .host(&cfg.host)
+            .port(cfg.port)
+            .username(&cfg.username)
+            .password(&cfg.password)
+            .database(&cfg.database)
+    }
+}
+
+fn connect_database_with(cfg: DatabaseConfig) -> PgPool {
+    PgPool::connect_lazy_with(cfg.into())
+}
+
+async fn health_check(State(pool): State<PgPool>) -> StatusCode {
+    match sqlx::query("SELECT 1").fetch_one(&pool).await {
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        Ok(_) => StatusCode::OK,
+    }
+}
+
+// #[tokio::test]
+// async fn test_health_check() {
+//     let response = health_check().await;
+//     assert_eq!(response, StatusCode::OK);
+// }
