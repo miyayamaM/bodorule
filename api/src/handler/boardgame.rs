b7f9bcc37efe2e239deb06418170dd4bcec412e0
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-10-12 10:09:35 +0900

    feat: api layer の実装

--- api/src/handler/boardgame.rs
+++ api/src/handler/boardgame.rs
@@ -1,11 +1,40 @@
-use axum::{extract::State, http::StatusCode};
+use axum::{
+    extract::{self, State},
+    http::StatusCode,
+};
+use domain::entity::boardgame::Boardgame;
 use domain::repository::boardgame::BoardgameRepository;
+use serde::Deserialize;
 use std::sync::Arc;
+use url::Url;
+use uuid::Uuid;
 
 use registry::AppModule;
 use shaku::HasComponent;
 
-pub async fn create_board_game(State(registry): State<Arc<AppModule>>) -> StatusCode {
+pub async fn create_board_game(
+    State(registry): State<Arc<AppModule>>,
+    extract::Json(payload): extract::Json<CreateBoardGameRequest>,
+) -> StatusCode {
     let boardgame_repository: &dyn BoardgameRepository = registry.resolve_ref();
+    println!("{:#?}", payload);
+    boardgame_repository.save(payload.try_into().unwrap()).await;
     StatusCode::OK
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CreateBoardGameRequest {
+    pub title: String,
+    pub thumbnail_url: Option<String>,
+}
+
+impl TryFrom<CreateBoardGameRequest> for Boardgame {
+    type Error = anyhow::Error;
+    fn try_from(value: CreateBoardGameRequest) -> Result<Self, Self::Error> {
+        Ok(Self {
+            id: Uuid::new_v4(),
+            title: value.title,
+            thumbnail_url: value.thumbnail_url.and_then(|v| Url::parse(&v).ok()),
+        })
+    }
 }
--- domain/src/entity/boardgame.rs
+++ domain/src/entity/boardgame.rs
@@ -2,7 +2,7 @@ use url::Url;
 use uuid::Uuid;
 
 pub struct Boardgame {
-    id: Uuid,
+    pub id: Uuid,
     pub title: String,
     pub thumbnail_url: Option<Url>,
 }
